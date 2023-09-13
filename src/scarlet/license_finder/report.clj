3cf822fc06eea7c0532f7c4c3a1f046762eedd4d
Vitaly Samigullin <vrs@pilosus.org>
2023-09-13 17:46:21 +0200

    Add clojure cli tools entrypoint

--- src/scarlet/license_finder/report.clj
+++ src/scarlet/license_finder/report.clj
@@ -3,25 +3,61 @@
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(defn- write-csv [file-path deps]
-  (with-open [writer (io/writer (fs/file file-path))]
-    (->> deps
-         (map (fn [{:keys [version type license]
-                    dep-name :name}]
-                [dep-name
-                 version
-                 (name type)
-                 (:id license)
-                 (:name license)
-                 (:url license)]))
-         (cons ["Dependency" "Version" "Type"
-                "License ID" "License Name" "License URL"])
-         (csv/write-csv writer))))
+(def headers ["Dependency" "Version" "Type" "License ID" "License Name" "License URL"])
 
-(defn write-report [{:keys [project out-file]} deps]
-  (let [out-file (or out-file
-                     (fs/normalize (fs/path "./target/licenses" (str project ".csv"))))]
-    (when-let [dir (fs/parent out-file)]
+(defn- dep->vec
+  "Return a flat vector with dependency's data out of a nested map"
+  [{:keys [version type license] dep-name :name}]
+  [dep-name
+   version
+   (name type)
+   (:id license)
+   (:name license)
+   (:url license)])
+
+(defn- prep-out-path
+  [{:keys [project format out-file]
+    :or   {format :csv}}]
+  (let [filename (str project "." (name format))
+        path (or out-file
+                 (->> filename
+                      (fs/path "./target/licenses")
+                      (fs/normalize)))]
+    (when-let [dir (fs/parent path)]
       (fs/create-dirs dir))
-    (write-csv out-file deps)
-    (println "Licenses written to" (str out-file))))
+    (println "Licenses written to" (str path))
+    path))
+
+(defn- prep-writer
+  "Get a io/writer for a given out-file.
+
+  Create a holding directory if it doesn't exist. The `out-file` value
+  `:stdout` is treated as a special value for printing to the standard
+  output."
+  [{:keys [out-file] :as options}]
+  (cond
+    (= out-file :stdout) {:writer *out* :close? false}
+    :else {:writer (-> options
+                       prep-out-path
+                       fs/file
+                       io/writer)
+           :close? true}))
+
+(defn- write-csv [options deps]
+  (let [{:keys [writer close?]} (prep-writer options)]
+    (->> deps
+         (map dep->vec)
+         (cons headers)
+         (csv/write-csv writer))
+    (when close?
+      (.close writer))))
+
+;; Support new report formats by adding new defmethods
+
+(defmulti generate-report
+  "Generate and write a report for a given format"
+  (fn [options _]
+    (get options :format)))
+
+(defmethod generate-report :default [options deps]
+  (write-csv options deps))
